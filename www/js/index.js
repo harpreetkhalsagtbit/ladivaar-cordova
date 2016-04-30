57e8b74e0fe2043e26165c929128d5b406147e96
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2016-04-30 20:29:27 +0530

    Fixunwanted page move while checking for pad-ched of th words at the corners of the screen. Due to overlapping of position o carousel and word

--- www/js/index.js
+++ www/js/index.js
@@ -36,7 +36,7 @@ var app = {
                     if (_res.length) {
                         var _baaniName = _res[1]
                         var _baani = nitnemBaani[_baaniName];
-                        ladivaarGenerator(_baani, $("#nitnenm_baani_container"), true)
+                        ladivaarGenerator(_baani, $("#nitnem_baani_container"), true)
                     }
                     showMe('nitnem_gurbani')
                 } else if (window.location.hash.match(/_ang_\d{1,4}/)) {
@@ -48,9 +48,9 @@ var app = {
                             ang--;
                             var _angData = data[ang];
                             var _baani = _angData.baani
-                            // baani_container
+                                // baani_container
                             ladivaarGenerator(_baani, $(".active [data-content]"), false)
-                            $('#angPositionFooter').html((ang+1)+"/1430")
+                            $('#angPositionFooter').html((ang + 1) + "/1430")
                         }
                         showMe('srigurugranthsahib_ang')
                     }
@@ -97,62 +97,64 @@ function ladivaarGenerator(baani, element, singleNumber) {
     // baani_container
     var _htmlString = "";
     var _paragraphStringHTMLArr = [];
-    for (var i = 0; i < baani.length; i++) {
-        var _paragraphsStr = null;
-        if(singleNumber) {
-            _paragraphsStr = baani[i].baani_content.replace(/([੦-੯]+॥){1,}/g, function($0) {
-                return $0 + "\n"
-            })
-        } else {
-            _paragraphsStr = baani[i].baani_content.replace(/([੦-੯]+॥){2,}/g, function($0) {
-                return $0 + "\n"
-            })
-        }
-        var _paragraphsArr = _paragraphsStr.trim().split(/\n/);
-        if (_paragraphsArr && _paragraphsArr.length) {
-            for (var paragraph = 0; paragraph < _paragraphsArr.length; paragraph++) {
-                var _wordsArr = _paragraphsArr[paragraph].split(/\s+/g);
-                if (_wordsArr.length) {
-                    if (baani[i].bold || baani[i].tab) {
-                        _paragraphStringHTMLArr.push("<h1 class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></h1>")
-                    } else {
-                        _paragraphStringHTMLArr.push("<p class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></p>")
+    if (baani) {
+        for (var i = 0; i < baani.length; i++) {
+            var _paragraphsStr = null;
+            if (singleNumber) {
+                _paragraphsStr = baani[i].baani_content.replace(/([੦-੯]+॥){1,}/g, function($0) {
+                    return $0 + "\n"
+                })
+            } else {
+                _paragraphsStr = baani[i].baani_content.replace(/([੦-੯]+॥){2,}/g, function($0) {
+                    return $0 + "\n"
+                })
+            }
+            var _paragraphsArr = _paragraphsStr.trim().split(/\n/);
+            if (_paragraphsArr && _paragraphsArr.length) {
+                for (var paragraph = 0; paragraph < _paragraphsArr.length; paragraph++) {
+                    var _wordsArr = _paragraphsArr[paragraph].split(/\s+/g);
+                    if (_wordsArr.length) {
+                        if (baani[i].bold || baani[i].tab) {
+                            _paragraphStringHTMLArr.push("<h1 class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></h1>")
+                        } else {
+                            _paragraphStringHTMLArr.push("<p class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></p>")
+                        }
                     }
                 }
-            }
-        } else {
-            console.log("Error", baani[i].baani_content)
-            var _wordsArr = baani[i].baani_content.split(/\s+/g);
-            if (_wordsArr.length) {
-                _paragraphStringHTMLArr.push("<p class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></p>")
+            } else {
+                console.log("Error", baani[i].baani_content)
+                var _wordsArr = baani[i].baani_content.split(/\s+/g);
+                if (_wordsArr.length) {
+                    _paragraphStringHTMLArr.push("<p class='text-center'><span>" + _wordsArr.join("</span><span>") + "</span></p>")
+                }
             }
         }
+        _htmlString = "<div class='baaniFormatting'>" + _paragraphStringHTMLArr.join("</div><div class='baaniFormatting'>") + "</div>"
+        $(element).html(_htmlString)
+
+        $("[data-attr='homeList']").hide();
+        $("[data-attr='tatkra']").hide()
+        $("[data-attr='srigurugranthsahib_ang']").show()
+
+        $('h1 span').mouseover(function() {
+            $(this).addClass('highlightH1')
+        });
+        $('h1 span').mouseout(function() {
+            $(this).removeClass('highlightH1')
+        });
+        $('p span').mouseover(function() {
+            $(this).addClass('highlightP')
+        });
+        $('p span').mouseout(function() {
+            $(this).removeClass('highlightP')
+        });
     }
-    _htmlString = "<div class='baaniFormatting'>" + _paragraphStringHTMLArr.join("</div><div class='baaniFormatting'>") + "</div>"
-    $(element).html(_htmlString)
-
-    $("[data-attr='homeList']").hide();
-    $("[data-attr='tatkra']").hide()
-    $("[data-attr='srigurugranthsahib_ang']").show()
-
-    $('h1 span').mouseover(function() {
-        $(this).addClass('highlightH1')
-    });
-    $('h1 span').mouseout(function() {
-        $(this).removeClass('highlightH1')
-    });
-    $('p span').mouseover(function() {
-        $(this).addClass('highlightP')
-    });
-    $('p span').mouseout(function() {
-        $(this).removeClass('highlightP')
-    });
 }
 
 var pageList = ['homeList', 'tatkra', 'srigurugranthsahib_ang', 'nitnem_tatkra', 'nitnem_gurbani']
 
 function showMe(me) {
-    for(var i=0;i<pageList.length;i++) {
+    for (var i = 0; i < pageList.length; i++) {
         $("[data-attr='" + pageList[i] + "']").hide()
     }
     $("[data-attr='" + me + "']").show()
@@ -160,17 +162,17 @@ function showMe(me) {
 
 var ang = 1
 var click = ""
-$('#carousel-example-generic').on('slide.bs.carousel', function () {
+$('#carousel-example-generic').on('slide.bs.carousel', function() {
     setTimeout(function() {
         var _angData = data[ang];
-        if( _angData) {
+        if (_angData) {
             var _baani = _angData.baani
-            // baani_container
+                // baani_container
             ladivaarGenerator(_baani, $(".active [data-content]"), false)
             // jQuery("div.item.active [data-content]").html(demo_data[ang-1]["content"])
             jQuery("div.item:not(.active) [data-content]").html("")
         }
-        $('#angPositionFooter').html((ang+1)+"/1430")
+        $('#angPositionFooter').html((ang + 1) + "/1430")
     }, 1000)
 })
 
@@ -192,18 +194,17 @@ $('a[data-slide="next"]').click(function() {
 
 $('#goToAngButton').click(function() {
     var _ang = parseInt($("#goToAngTextBox").val());
-    if(!isNaN(_ang) && _ang > 0 && _ang <= 1430) {
+    if (!isNaN(_ang) && _ang > 0 && _ang <= 1430) {
         window.location.hash = "srigurugranthsahibjee_ang_" + _ang
     }
     $('#myModal').modal('hide');
     $("#goToAngTextBox").val("")
 })
 
-function minmax(value, min, max) 
-{
-    if(parseInt(value) < min || isNaN(value)) 
-        return 0; 
-    else if(parseInt(value) > max) 
-        return 1430; 
+function minmax(value, min, max) {
+    if (parseInt(value) < min || isNaN(value))
+        return 0;
+    else if (parseInt(value) > max)
+        return 1430;
     else return value;
 }
